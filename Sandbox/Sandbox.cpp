fca32c5801730ae67a6c66ea3fb764b4f24bc3ca
verodicia <verodiciathenaturegoddess@gmail.com>
2020-03-27 00:30:25 +0430

    minor changes

--- Sandbox/Sandbox.cpp
+++ Sandbox/Sandbox.cpp
@@ -6,17 +6,17 @@ class Sandbox : public Light::Application
 public:
 	Sandbox()
 	{
-		// Create window data
+		// create window data
 		Light::WindowData wd;
 		wd.title = "Demo";
 		wd.displayMode = Light::DisplayMode::Windowed;
 
-		// Create graphics configurations
+		// create graphics configurations
 		Light::GraphicsConfigurations gc;
 		gc.resolution = Light::Resolution(800.0f, 600.0f, Light::AspectRatio::AR_4_3);
 		gc.vSync = true;
 
-		// Create window and set callback function
+		// create window and set callback function
 		m_Window = std::make_unique<Light::Window>(wd, gc, Light::GraphicsAPI::Default);
 		m_Window->SetEventCallbackFunction(LT_EVENT_FN(Light::Application::OnEvent));
 	}
